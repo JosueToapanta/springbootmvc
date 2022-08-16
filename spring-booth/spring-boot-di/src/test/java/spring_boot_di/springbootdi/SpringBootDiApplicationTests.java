66aa0eb5a98e9a22bf51ae57db64ee1000def3ad
Josué Toapanta <josuetoapantatorres@gmail.com>
2022-08-16 16:56:52 -0500

    error

---
+++ spring-booth/spring-boot-di/src/test/java/spring_boot_di/springbootdi/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_di.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
